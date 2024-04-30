7d22d89ea9c5f5516577f5f064b2fcb0b7a40e21
Brian Kimurgor <kimurgorbrian20@gmail.com>
2024-04-30 22:44:59 +0300

    javascript node

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/**
+ * function name: displayMessage
+ * prints the string name
+*/
+
+function displayMessage(message){
+    console.log(message);
+}
+
+module.exports = displayMessage;
